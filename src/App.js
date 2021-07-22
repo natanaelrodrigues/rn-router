8fcd23838c22f472848ec4747558c72f53f9ceda
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2021-07-22 15:20:46 -0300

    page 404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
-import Home from './pages/Home'
-import Sobre from './pages/Sobre'
-import Categoria from './pages/Categoria'
-import Produto from './pages/Produto'
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Categoria from "./pages/Categoria";
+import Produto from "./pages/Produto";
 
-function App(){
+function App() {
   return (
     <BrowserRouter>
       <header>
@@ -31,19 +31,21 @@ function App(){
             <li>
               <Link to="/Produto?p=9988585">Produto</Link>
             </li>
+            <li>
+              <Link to="/kljçlkj">Vai dar erro/PT se clicar aqui</Link>
+            </li>
           </ul>
         </nav>
       </header>
-      <hr/>
+      <hr />
 
       <Switch>
-
         <Route exact path="/">
-          <Home/>
+          <Home />
         </Route>
 
         <Route exact path="/sobre">
-          <Sobre/>
+          <Sobre />
         </Route>
 
         <Route path="/categoria/:cat/:id">
@@ -54,13 +56,13 @@ function App(){
           <Produto />
         </Route>
 
+        <Route path="*">
+          <h1>404 - Page not Found!!!</h1>
+        </Route>
       </Switch>
 
-      <hr/>
-      <footer>
-        Todos os Direitos Reservados
-      </footer>
-
+      <hr />
+      <footer>Todos os Direitos Reservados</footer>
     </BrowserRouter>
   );
 }
